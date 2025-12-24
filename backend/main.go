d922458042bc46a8a1f387a49374755d6bf81f2d
khiem <khiemnguyen.hye@gmail.com>
2025-12-24 23:37:25 +0700

    Add healthcheck

--- backend/main.go
+++ backend/main.go
@@ -2,12 +2,28 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"log"
+	"os"
 )
 
 func main() {
 	fmt.Print("Hello from the backend!")
-	
+	godotenv.Load()
+
+	app := gin.Default()
+
+	app.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "healthy",
+		})
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT is undefined")
+	}
+
+	app.Run(":" + port)
 }
