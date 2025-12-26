38a4c828bb094c9f9788c2c23d1206658a8da49b
Anh D Tran <tran3ah@mail.uc.edu>
2025-12-26 01:11:37 -0500

    Initializing main.go

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"katanaid/handlers"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
 
 func main() {
-	fmt.Print("Hello from the backend!")
+	// Initialize router
+	r := chi.NewRouter()
+
+	// Register health endpoint
+	r.Get("/health", handlers.Health)
+
+	// Start server
+	fmt.Println("Server is running on port 8080")
+	http.ListenAndServe(":8080", r)
 }
