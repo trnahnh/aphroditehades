3af0a76660f02b7d761b3bc51bd7014d1048a95b
khiem <khiemnguyen.hye@gmail.com>
2025-12-27 03:33:42 +0700

    Refine landing page button hover effect.

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,14 +1,14 @@
 import { Button } from "@/components/ui/button"
-import { LucideArrowRight, LucideSearch, LucideSearchCheck, LucideSearchCode } from "lucide-react"
+import { LucideSearch, LucideSearchCode } from "lucide-react"
 
 const LandingPage = () => {
   return (
     <>
       <div className="flex flex-col justify-center pt-20 items-center font-bold text-7xl">
         <Button variant={"outline"} className="group overflow-hidden rounded-2xl">
-          <LucideSearch className="group-hover:translate-x-40 transition-all" />
+          <LucideSearch className="group-hover:hidden transition-opacity" />
           Check for deepfake
-          <LucideSearchCheck className="hidden group-hover:block mr-2 transition-all"/>
+          <LucideSearchCode className="hidden group-hover:block mr-2 transition-all"/>
         </Button>
         <h1 className="mt-10">Detect Deepfake Content</h1>
         <h1 className="mt-5">with Precision</h1>
