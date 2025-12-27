0c599a65d32abd7d9f9d51e349d3d7ddf9b57d4d
khiem <khiemnguyen.hye@gmail.com>
2025-12-28 06:27:01 +0700

    Configure API URL with environment variables.

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 })
